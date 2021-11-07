9b7d20a62940abdee17b925120de60d1b8d52fa1
pratpongwork <pratpong.work@gmail.com>
2021-11-07 13:47:26 +0700

    Update display.dart

--- lib/display.dart
+++ lib/display.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Display extends StatefulWidget {
   const Display({Key? key}) : super(key: key);
@@ -8,8 +10,24 @@ class Display extends StatefulWidget {
 }
 
 class _DisplayState extends State<Display> {
+  CollectionReference _studentCollection =
+      FirebaseFirestore.instance.collection("students");
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Show"),
+      ),
+      body: StreamBuilder(
+          stream: _studentCollection.snapshots(),
+          builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (!snapshot.hasData) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          }),
+    );
   }
 }
