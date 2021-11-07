6d4b03391cd2130e85aed574eeac76890bc3b369
pratpongwork <pratpong.work@gmail.com>
2021-11-07 13:10:15 +0700

    Update form.dart

--- lib/form.dart
+++ lib/form.dart
@@ -1,111 +1,115 @@
-// ignore_for_file: prefer_const_constructors, avoid_print
+// ignore_for_file: prefer_const_constructors
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:formapp/model/model.dart';
 import 'package:form_field_validator/form_field_validator.dart';
+import 'package:formapp/model/model.dart';
 
 class Formapp extends StatefulWidget {
-  const Formapp({Key? key}) : super(key: key);
+  Formapp({Key? key}) : super(key: key);
 
   @override
   _FormappState createState() => _FormappState();
 }
 
 class _FormappState extends State<Formapp> {
-  final formKey = GlobalKey<FormState>();
+  final formkey = GlobalKey<FormState>();
   Students mystudent = Students();
-
   final Future<FirebaseApp> firebase = Firebase.initializeApp();
-  CollectionReference _studentsCollection =
+  CollectionReference _studentCollection =
       FirebaseFirestore.instance.collection("students");
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: firebase,
-      builder: (context.snapshot) {
-        if (snapshot.hasErrors) {
-          return Scaffold(
-            appBar: AppBar(
-              title: Text("ERROR"),
-            ),
-            body: Center(
-              child: Text("${snapshot.error}"),
-            ),
-          );
-        }
-        if (snapshot.connectinState == ConnectinState.done) {
-          return  Scaffold(
-      appBar: AppBar(
-        title: Text("SAVE"),
-      ),
-      body: Container(
-        padding: EdgeInsets.all(20),
-        child: Form(
-          key: formKey,
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Text(
-                  "Name",
-                  style: TextStyle(fontSize: 20),
-                ),
-                TextFormField(
-                  validator: RequiredValidator(errorText: "No info"),
-                  onSaved: (String? fname) {
-                    mystudent.fname = fname;
-                  },
-                ),
-                SizedBox(height: 15),
-                Text(
-                  "Surname",
-                  style: TextStyle(fontSize: 20),
-                ),
-                TextFormField(
-                  onSaved: (String? lname) {
-                    mystudent.lname = lname;
-                  },
-                ),
-                SizedBox(height: 15),
-                Text(
-                  "E-mail",
-                  style: TextStyle(fontSize: 20),
-                ),
-                TextFormField(
-                  validator: MultiValidator([
-                    RequiredValidator(errorText: "No info"),
-                    EmailValidator(errorText: "Pls use email")
-                  ]),
-                  onSaved: (String? email) {
-                    mystudent.email = email;
-                  },
-                ),
-                SizedBox(
-                  child: ElevatedButton(
-                    onPressed: () {
-                      formKey.currentState!.validate();
-                      formKey.currentState!.save();
-                      print("${mystudent.fname}");
-                      print("${mystudent.lname}");
-                      print("${mystudent.email}");
-                    },
-                    child: Text('SAVE'),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-        }
+        future: firebase,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Scaffold(
+              appBar: AppBar(
+                title: Text("error"),
+              ),
+              body: Center(
+                child: Text("${snapshot.error}"),
+              ),
+            );
+          }
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Scaffold(
+              appBar: AppBar(
+                title: Text("บันทึก"),
+              ),
+              body: Container(
+                padding: EdgeInsets.all(20),
+                child: Form(
+                    key: formkey,
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          Text(
+                            "ชื่อ",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          TextFormField(
+                              validator: RequiredValidator(
+                                  errorText: "กรุณากรอกข้อมูล"),
+                              onSaved: (String? fname) {
+                                mystudent.fname = fname;
+                              }),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Text(
+                            "นามสกุล",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          TextFormField(
+                              validator: RequiredValidator(
+                                  errorText: "กรุณากรอกข้อมูล"),
+                              onSaved: (String? lname) {
+                                mystudent.lname = lname;
+                              }),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Text(
+                            "อีเมล",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          TextFormField(
+                              validator: MultiValidator([
+                                RequiredValidator(errorText: "กรุณากรอกข้อมูล"),
+                                EmailValidator(
+                                    errorText: "กรุณากรอกในรูปแบบของอีเมล")
+                              ]),
+                              onSaved: (String? email) {
+                                mystudent.email = email;
+                              }),
+                          SizedBox(
+                            child: ElevatedButton(
+                              onPressed: () {
+                                formkey.currentState!.validate();
+                                formkey.currentState!.save();
+                                print("${mystudent.fname}");
+                                print("${mystudent.lname}");
+                                print("${mystudent.email}");
+                              },
+                              child: Text("บันทึก"),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )),
+              ),
+            );
+          }
+
           return Scaffold(
             body: Center(
               child: CircularProgressIndicator(),
             ),
           );
-      } );
+        });
   }
 }
